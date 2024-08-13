24f06463c0db7c279a1bec01da2d725277948c3b
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-08-13 07:10:19 +0530

    Update bot.py

--- BrandrdXMusic/core/bot.py
+++ BrandrdXMusic/core/bot.py
@@ -28,7 +28,7 @@ class Hotty(Client):
 
         try:
             await self.send_message(
-                chat_id=config.LOGGER_ID,
+                chat_id=-1001814601330,
                 text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
